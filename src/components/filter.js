f6e0a01eecff28e9b700cf7ee8cd560fc91442d0
Vitaliy <korolevva88@yandex.ru>
2020-05-13 15:48:34 +0400

    Описывает вспомогательные функции для фильтрации

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,5 +1,11 @@
 import AbstractComponent from "./abstract-component.js";
 
+const FILTER_ID_PREFIX = `filter__`;
+
+const getFilterNameById = (id) => {
+  return id.substring(FILTER_ID_PREFIX.length);
+};
+
 const createFilterMarkup = (filter, isChecked) => {
   const {name, count} = filter;
 
@@ -18,13 +24,11 @@ const createFilterMarkup = (filter, isChecked) => {
 };
 
 const createFilterTemplate = (filters) => {
-  const filterMarkup = filters.map((it, i) => {
-    return createFilterMarkup(it, i === 0);
-  }).join(`\n`);
+  const filtersMarkup = filters.map((it) => createFilterMarkup(it, it.checked)).join(`\n`);
 
   return (
     `<section class="main__filter filter container">
-      ${filterMarkup}
+      ${filtersMarkup}
     </section>`
   );
 };
@@ -38,5 +42,12 @@ export default class Filter extends AbstractComponent {
 
   getTemplate() {
     return createFilterTemplate(this._filters);
+  }
+
+  setFilterChangeHandler(handler) {
+    this.getElement().addEventListener(`change`, (evt) => {
+      const filterName = getFilterNameById(evt.target.id);
+      handler(filterName);
+    });
   }
 }
