21baf8f919871b99fb96b93b33709183412b2dd1
Vitaliy <korolevva88@yandex.ru>
2020-05-13 18:27:57 +0400

    Делает рефакторинг utils.js

--- src/components/task.js
+++ src/components/task.js
@@ -1,6 +1,6 @@
 import AbstractComponent from "./abstract-component.js";
 import {formatTime, formatDate} from "../utils/common.js";
-
+import {isOverdueDate} from "../utils/common.js";
 
 const createButtonMarkup = (name, isActive = true) => {
   return (
@@ -16,7 +16,7 @@ const createButtonMarkup = (name, isActive = true) => {
 const createTaskTemplate = (task) => {
   const {description, dueDate, color, repeatingDays} = task;
 
-  const isExpired = dueDate instanceof Date && dueDate < Date.now();
+  const isExpired = dueDate instanceof Date && isOverdueDate(dueDate, new Date());
   const isDateShowing = !!dueDate;
 
   const date = isDateShowing ? formatDate(dueDate) : ``;
